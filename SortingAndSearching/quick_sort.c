0f52c6742459c4b11ba105078e94907db0420fa4
programworld999 <programworld999@gmail.com>
2020-03-13 21:26:54 +0530

    update

--- SortingAndSearching/quick_sort.c
+++ SortingAndSearching/quick_sort.c
@@ -8,22 +8,7 @@ void swap(int *a, int *b)
   *b = t;
 }
 
-// int partition(int data[], int start, int end)
-// {
-//     int pivot = data[end];
-//     int i = start-1;
 
-//   for (int j = start; j < end; j++){
-//     if (data[j] <= pivot)
-//     {
-//       i++;
-//       swap(&data[i], &data[j]);
-//     }
-//   }
-//   swap(&data[i + 1], &data[end]);
-//   return (i + 1);
-
-// }
 int partition(int data[], int start, int end){
     int pivot = data[end];
     int i = start-1;
